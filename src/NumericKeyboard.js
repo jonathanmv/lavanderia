d3d151653e7501f6fc4b315b8604345ce7f3eaae
Jonathan Morales Vélez <jonathan@themidgame.com>
2017-12-20 15:44:38 +0100

    Improve NumericKeyboard

--- src/NumericKeyboard.js
+++ src/NumericKeyboard.js
@@ -2,17 +2,41 @@ import './NumericKeyboard.css'
 
 import React, { Component } from 'react'
 
+import { Display1 } from './Texts'
+
 import Grid from 'material-ui/Grid'
 import Button from 'material-ui/Button'
+import BackspaceIcon from 'material-ui-icons/Backspace'
+import DoneIcon from 'material-ui-icons/Done'
+import ClearIcon from 'material-ui-icons/Clear'
 
 export default class NumericKeyboard extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      numbers: []
+    }
+  }
+
   onKeyPressed = event => {
-    if (this.props.onKeyPressed) {
+    const number = event.target.name || event.target.parentElement.name
+    this.setState({ numbers: [...this.state.numbers, Number(number)] })
+  }
+
+  removeLastNumber = () => {
+    this.setState({ numbers: this.state.numbers.slice(0, -1)})
+  }
+
+  removeAllNumbers = () => {
+    this.setState({ numbers: [] })
+  }
+
+  dispatchOnNumber = () => {
+    if (this.props.onNumber) {
       try {
-        const number = event.target.name || event.target.parentElement.name
-        this.props.onKeyPressed(Number(number))
+        this.props.onNumber(this.state.numbers.join(''))
       } catch (error) {
-        console.log(error)
+        console.error(error)
       }
     }
   }
@@ -20,6 +44,23 @@ export default class NumericKeyboard extends Component {
   render() {
     return (
       <Grid container spacing={0} alignItems="stretch">
+        <Grid item xs={12}>
+          <Grid container>
+            <Grid item>
+              <Button style={{ touchAction: 'none', width: '100%', height: '100%' }} aria-label="Clear" onClick={this.removeAllNumbers}>
+                <ClearIcon />
+              </Button>
+            </Grid>
+            <Grid item style={{ textAlign: 'center', flex: 1 }}>
+              <Display1>{this.state.numbers.join('')}</Display1>
+            </Grid>
+            <Grid item>
+              <Button style={{ touchAction: 'none', width: '100%', height: '100%' }} aria-label="Backspace" onClick={this.removeLastNumber}>
+                <BackspaceIcon />
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
         <Grid item xs={12}>
           <Grid container spacing={0} justify="center" alignItems="stretch">
             {[1,2,3].map(i => <Grid item xs={4} key={i}><Button name={i} onClick={this.onKeyPressed} className="NumericKeyboard-Button" raised color="accent">{i}</Button></Grid>)}
@@ -39,6 +80,11 @@ export default class NumericKeyboard extends Component {
           <Grid container spacing={0} justify="center" alignItems="stretch">
             {[0].map(i => <Grid item xs={4} key={i}><Button name={i} onClick={this.onKeyPressed} className="NumericKeyboard-Button" raised color="accent">{i}</Button></Grid>)}
           </Grid>
+        </Grid>
+        <Grid item xs={12} style={{ textAlign: 'center'}}>
+          <Button fab color="primary" onClick={this.dispatchOnNumber}>
+            <DoneIcon />
+          </Button>
         </Grid>
       </Grid>
     )
--- src/QueryItems.js
+++ src/QueryItems.js
@@ -5,22 +5,17 @@ import NumericKeyboard from './NumericKeyboard'
 import { Display1 } from './Texts'
 
 import Grid from 'material-ui/Grid'
-import Button from 'material-ui/Button'
-import BackspaceIcon from 'material-ui-icons/Backspace'
-import SearchIcon from 'material-ui-icons/Search'
-import ClearIcon from 'material-ui-icons/Clear'
 
 export default class QueryItems extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      code: [],
+      code: null,
       item: null
     }
   }
 
-  queryItem = async () => {
-    const code = this.state.code.join('')
+  queryItem = async code => {
     const { userId } = this.props
     try {
       const item = await api.getUserItem(userId, code)
@@ -28,18 +23,6 @@ export default class QueryItems extends Component {
     } catch (error) {
       console.log(error)
     }
-  }
-
-  onNumber = number => {
-    this.setState({ code: [...this.state.code, number] })
-  }
-
-  removeLastNumber = () => {
-    this.setState({ code: this.state.code.slice(0, -1)})
-  }
-
-  removeAllNumbers = () => {
-    this.setState({ code: [] })
   }
 
   render() {
@@ -56,29 +39,7 @@ export default class QueryItems extends Component {
         <Grid item>
           <Grid container direction="column">
             <Grid item>
-              <Grid container>
-                <Grid item>
-                  <Button style={{ touchAction: 'none', width: '100%', height: '100%' }} aria-label="Clear" onClick={this.removeAllNumbers}>
-                    <ClearIcon />
-                  </Button>
-                </Grid>
-                <Grid item style={{ textAlign: 'center', flex: 1 }}>
-                  <Display1>{this.state.code.join('')}</Display1>
-                </Grid>
-                <Grid item>
-                  <Button style={{ touchAction: 'none', width: '100%', height: '100%' }} aria-label="Backspace" onClick={this.removeLastNumber}>
-                    <BackspaceIcon />
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <NumericKeyboard onKeyPressed={this.onNumber} />
-            </Grid>
-            <Grid item style={{ textAlign: 'center'}}>
-              <Button fab color="primary" onClick={this.queryItem}>
-                <SearchIcon />
-              </Button>
+              <NumericKeyboard onNumber={this.queryItem} />
             </Grid>
           </Grid>
         </Grid>
